7b253457b5736a6418594a97a2972593316ba195
Pari Nannapaneni <pari@Paris-MacBook-Pro.local>
2017-04-21 12:22:20 +0530

    create dir and file if needed

--- myDevBkp.php
+++ myDevBkp.php
@@ -313,9 +313,11 @@ if( array_key_exists('useconfig', $arguments) ){
 			if( !is_dir($holdingdir) ){
 				mkdir( $holdingdir );
 			}
-			if(!file_exists( $arguments['opf'] )){
-				touch($arguments['opf'] );
+			if(file_exists( $arguments['opf'] )){
+				unlink($arguments['opf']);
 			}
+			touch($arguments['opf'] );
+			$opf = $arguments['opf'] ;
 		}else{
 			$opf = $pwd.$arguments['opf'] ;				
 		}
