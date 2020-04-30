526fcb3039a1afe672089552923ddfd3628ed3d7
Pari Nannapaneni <paripurnachand@gmail.com>
2020-04-30 08:10:47 +0530

    save point

--- myDevBkp.php
+++ myDevBkp.php
@@ -198,21 +198,21 @@ if( count($arguments) == 0 || (count($arguments) == 1 &&  array_key_exists('help
 	echo "\n--useconfig=FILE --sh=HOST --su=USER --sp=PASSWORD --opf=dump.sql {--locktables='Y'} 
 	Parses config FILE and connects to source host 'sh'
 	and dumps all backup data into a single dump sql output file 
-	while applying where conditions on each table - as configured in useconfing $FILE
+	while applying where conditions on each table - as configured in useconfing FILE
 	\n\n";
 
 
 	echo "\n--useconfig=FILE --sh=HOST --su=USER --sp=PASSWORD --fullbackup=BACKUP_PATH {--locktables='Y'} 
 	Parses config FILE and connects to source host 'sh'
-	and dumps all backup data into a individual backups sql files under folder $BACKUP_PATH
-	while applying where conditions on each table - as configured in useconfing $FILE 
+	and dumps all backup data into a individual backups sql files under folder BACKUP_PATH
+	while applying where conditions on each table - as configured in useconfing FILE 
 	\n\n";
 
 
 	echo "\n--useconfig=FILE --sh=HOST --su=USER --sp=PASSWORD --dh=HOST --du=USER --dp=PASSWORD {--locktables='Y'} 
 	Parses config FILE and connects to source host 'sh'
-	and dumps all backup data to another destination mysql-host $dh 
-	while applying where conditions on each table - as configured in useconfing $FILE 
+	and dumps all backup data to another destination mysql-host dh 
+	while applying where conditions on each table - as configured in useconfing FILE 
 	\n\n";
 
 }
